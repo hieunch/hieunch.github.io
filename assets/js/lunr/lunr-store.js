493d86c6547cb7e740a042c659ab3385c268471f
hieunch <chihieu2506@gmail.com>
2025-01-03 18:19:50 +1100

    first commit

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "IBM Parity Challenge",
+        "excerpt":"The article details my winning solution for the IBM Parity challenge. Please refer to the challenge description page for comprehensive details, including the objective, input/output format, evaluation metrics, and other requirements. Author: Chi-Hieu Nguyen, University of Technology Sydney, Australia. Overview of the approach The parity function $parity(x) = x \\mod...","categories": [],
+        "tags": ["FHERMA Challenge","Homomorphic Encryption","IBM Research","OpenFHE"],
+        "url": "/posts/fherma-challenge/parity/",
+        "teaser": "/assets/images/placeholder.jpg"
+      },{
+        "title": "CIFAR-10 Encrypted Image Classification",
+        "excerpt":"The article details my winning solution for the FHERMA CIFAR-10 challenge. Please refer to the challenge description page for comprehensive details, including the objective, input/output format, evaluation metrics, and other requirements. Author: Chi-Hieu Nguyen, University of Technology Sydney, Australia. Introduction The challenge task involves inferring the class of an encrypted...","categories": [],
+        "tags": ["FHERMA Challenge","Homomorphic Encryption","Privacy-preserving ML","OpenFHE"],
+        "url": "/posts/fherma-challenge/cifar10/",
+        "teaser": "/assets/images/placeholder.jpg"
+      },{
+        "title": "Bitwise Shift Left Challenge",
+        "excerpt":"The article details my winning solution for the FHERMA SHL challenge. Please refer to the challenge description page for comprehensive details, including the objective, input/output format, evaluation metrics, and other requirements. Author: Chi-Hieu Nguyen, University of Technology Sydney, Australia. Introduction The objective of the challenge is to evaluate a bitwise...","categories": [],
+        "tags": ["FHERMA Challenge","Homomorphic Encryption","OpenFHE"],
+        "url": "/posts/fherma-challenge/shl/",
+        "teaser": "/assets/images/placeholder.jpg"
+      },{
+        "title": "IBM Encrypted Array Sorting Challenge",
+        "excerpt":"The article details my winning solution for the IBM Array Sorting challenge. Please refer to the challenge description page for comprehensive details, including the objective, input/output format, evaluation metrics, and other requirements. Author: Chi-Hieu Nguyen, University of Technology Sydney, Australia. Introduction The objective of the challenge is to sort an...","categories": [],
+        "tags": ["FHERMA Challenge","Homomorphic Encryption","IBM Research","OpenFHE"],
+        "url": "/posts/fherma-challenge/sorting/",
+        "teaser": "/assets/images/placeholder.jpg"
+      }]
